0f71c14644fa6f66bb67a3a6fddc44748217884d
Benker-Leung <564004820@qq.com>
2018-01-07 17:10:21 +0800

    nothing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('public'));
+ app.use('/', express.static('public'));
 
 app.listen(3000);
 
 app.get('/', function(req, res){
   res.sendFile('./public/index.html');
-  res.end();
 });
